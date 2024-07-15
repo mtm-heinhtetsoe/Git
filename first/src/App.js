5276d42cdfb39fc9409b6a87bbcec994bd088d9c
HeinHtetSoe-SCM <scm.heinhtetsoe@gmail.com>
2024-07-15 14:16:09 +0630

    react basic

--- first/src/App.js
+++ first/src/App.js
@@ -1,5 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Application extends React.Component {
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+
+  state = {
+    items: [
+      {id: 1, name:"Apple", price:"3"},
+      {id: 2, name:"Banana", price:"2"}
+    ]
+  }
+
+  add = () => {
+    let id = this.state.items.length + 1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+
+    this.setState({
+      items: [
+        ...this.state.items,
+        {id, name, price}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <ul>
+          {
+            this.state.items.map(i => <Item key={i.id} name={i.name} price={i.price} />)
+          }
+        </ul>
+        <input type='text' ref={this.nameRef} /><br />
+        <input type='text' ref={this.priceRef} /><br />
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+}
+
+class Item extends React.Component {
+  render() {
+    return <li>{this.props.name}, ${this.props.price}</li>
+  }
+}
 
 function App() {
   return (
@@ -22,4 +71,4 @@ function App() {
   );
 }
 
-export default App;
+export default Application;
