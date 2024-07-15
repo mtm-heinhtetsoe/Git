e473e8ab8d38ae87da0e90cae450e57ea0b52284
HeinHtetSoe-SCM <scm.heinhtetsoe@gmail.com>
2024-07-15 15:49:09 +0630

    change to functional components

--- first/src/App.js
+++ first/src/App.js
@@ -1,92 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-class Application extends React.Component {
+const App = () => {
+  let [items, setItems] = useState([
+    {id: 1, name:"Apple", price:"3"},
+    {id: 2, name:"Banana", price:"2"}
+  ])
 
-  nameRef = React.createRef();
-  priceRef = React.createRef();
+  let add = (name, price) => {
+    let id = items.length + 1;
 
-  state = {
-    items: [
-      {id: 1, name:"Apple", price:"3"},
-      {id: 2, name:"Banana", price:"2"}
-    ]
+    setItems([
+      ...items,
+      {id, name, price}
+    ])
   }
 
-  add = (name, price) => {
-    let id = this.state.items.length + 1;
-
-    this.setState({
-      items: [
-        ...this.state.items,
-        {id, name, price}
-      ]
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello React</h1>
-        <ul>
-          {
-            this.state.items.map(i => <Item key={i.id} name={i.name} price={i.price} />)
-          }
-        </ul>
-        <AddForm add={this.add} />
-      </div>
-    );
-  }
-}
-
-class AddForm extends React.Component {
-  nameRef = React.createRef();
-  priceRef = React.createRef();
-
-  add = () => {
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
-
-    this.props.add(name, price);
-  }
-
-  render() {
-    return (
-      <div>
-        <input type='text' ref={this.nameRef} /><br />
-        <input type='text' ref={this.priceRef} /><br />
-        <button onClick={this.add}>Add</button>
-      </div>
-    )
-  }
-}
-
-class Item extends React.Component {
-  render() {
-    return <li>{this.props.name}, ${this.props.price}</li>
-  }
-}
-
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React</h1>
+      <ul>{items.map(i => <Item key={i.id} name={i.name} price={i.price} />)}</ul>
+      <AddForm add={add} />
     </div>
   );
 }
 
-export default Application;
+const AddForm = props => {
+  let nameRef = React.createRef();
+  let priceRef = React.createRef();
+
+  let add = () => {
+    let name = nameRef.current.value;
+    let price = priceRef.current.value;
+
+    props.add(name, price);
+  }
+
+  return (
+    <div>
+      <input type='text' ref={nameRef} /><br />
+      <input type='text' ref={priceRef} /><br />
+      <button onClick={add}>Add</button>
+    </div>
+  )
+}
+
+const Item = props => <li>{props.name}, ${props.price}</li>;
+
+export default App;
